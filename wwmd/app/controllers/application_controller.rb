7e25d2e50c7fe9ed0003d680f53d81238f4cea83
Miriam Hernandez <miriamhz14@gmail.com>
2017-01-19 12:04:07 -0500

    most routes done and style

--- wwmd/app/controllers/application_controller.rb
+++ wwmd/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # strong parameters to protect database while at the same time allowing for updates
+  # referencing built in methods
+  protected
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :email, :password, :password_confirmation, :remember_me])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+  end
 end
+
--- wwmd/app/controllers/blog_controller.rb
+++ wwmd/app/controllers/blog_controller.rb
@@ -1,10 +1,21 @@
 class BlogController < ApplicationController
+
+
   def index
   end
 
   def home
+    @posts = Post.all
+    @newPost = Post.new
   end
 
   def profile
+    if (User.find_by_username(params[:id]))
+    @username = params[:id]
+      else
+        redirect_to root_path, :notice=> "User Not Found"
+    end
+      @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+      @newPost = Post.new
   end
 end
